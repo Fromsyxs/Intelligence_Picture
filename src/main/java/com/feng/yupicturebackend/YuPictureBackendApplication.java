b2e2f5ba82204eb723081229956a75c48e9bdd80
缘在风里 <1529846583@qq.com>
2025-07-08 22:56:48 +0800

    feat：新建云图库项目

---
+++ src/main/java/com/feng/yupicturebackend/YuPictureBackendApplication.java
@@ -0,0 +1,16 @@
+package com.feng.yupicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 缘在风里
+ */
+@SpringBootApplication
+public class YuPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuPictureBackendApplication.class, args);
+    }
+
+}
