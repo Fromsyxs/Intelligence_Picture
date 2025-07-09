51f1fab988b15a42d18adc9f0fbbe4bec077aa3f
缘在风里 <1529846583@qq.com>
2025-07-10 00:29:38 +0800

    feat：新建云图库项目

---
+++ src/main/java/com/feng/yupicturebackend/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.feng.yupicturebackend.controller;
+
+import com.feng.yupicturebackend.common.BaseResponse;
+import com.feng.yupicturebackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 缘在风里
+ *
+ */
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
